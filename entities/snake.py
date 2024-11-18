bc246067feadefc824b91df3782d35f4f69a7092
ManoSoraDev <thiagogamer10000@gmail.com>
2024-11-17 23:38:20 -0300

    Modifiquei a funcao aumentar_tamanho

--- entities/snake.py
+++ entities/snake.py
@@ -1,47 +1,37 @@
 import pygame as py
 grid = 20 # Tamanho de cada bloco na tela
 
-class Snake():
-    def __init__(self):
-        self.corpo = [(120, 120), (90, 120), (80, 120)] ## A cobrinha está dividida em três segmentos - A medida que o jogo avança,
+class Snake:
+    def __init__(self, corpo):
+        self.corpo = [(0, 0), (0, 0), (0, 0)] ## A cobrinha está dividida em três segmentos - A medida que o jogo avança,
                                                         ## o número de segmentos aumenta;
                                                         ## Cada segmento é representado por coordenadas x e y;
                                                         ## A cabeça da cobra está na posição [0].
         self.direcao = 'RIGHT'
+        self.nova_cabeca = None
         self.cor_corpo = (0, 180, 0)
 
     def movimento(self): ## Seta os movimentos da cobrinha
         cabeca_x, cabeca_y = self.corpo[0] ## x e y aqui representam a coordenada da cabeça (lá ele).
 
-        nova_cabeca = (cabeca_x, cabeca_y) ## variável que armarzana o input do evento.
+        self.nova_cabeca = (cabeca_x, cabeca_y) ## variável que armarzana o input do evento.
 
         if self.direcao == 'RIGHT':
-            nova_cabeca = (cabeca_x + grid, cabeca_y)
+            self.nova_cabeca = (cabeca_x + grid, cabeca_y)
         elif self.direcao == 'LEFT':
-            nova_cabeca = (cabeca_x - grid, cabeca_y)
+            self.nova_cabeca = (cabeca_x - grid, cabeca_y)
         elif self.direcao == 'UP':
-            nova_cabeca = (cabeca_x, cabeca_y - grid)
+            self.nova_cabeca = (cabeca_x, cabeca_y - grid)
         elif self.direcao == 'DOWN':
-            nova_cabeca = (cabeca_x, cabeca_y + grid)
+            self.nova_cabeca = (cabeca_x, cabeca_y + grid)
 
-        self.corpo = [nova_cabeca] + self.corpo[:-1] ## Adiciona a nova posição captada pelo evento e apaga a última tupla
+        self.corpo = [self.nova_cabeca] + self.corpo[:-1] ## Adiciona a nova posição captada pelo evento e apaga a última tupla
 
     def cobra_tela(self, screen): ##Desenha a cobrinha na tela!
         for pos in self.corpo:
             py.draw.rect(screen, self.cor_corpo, py.Rect(pos[0], pos[1], grid, grid))
 
     def aumentar_tamanho(self):
-        cabeca_x, cabeca_y = self.corpo[0]
+        self.corpo = [self.nova_cabeca] + self.corpo
 
-        if self.direcao == "RIGHT": 
-            nova_cabeca = (cabeca_x + grid, cabeca_y)
-        elif self.direcao == "LEFT":
-            nova_cabeca = (cabeca_x - grid, cabeca_y)
-        elif self.direcao == "UP":
-            nova_cabeca = (cabeca_x, cabeca_y - grid)
-        elif self.direcao == "DOWN":
-            nova_cabeca = (cabeca_x, cabeca_y + grid)
-
-        self.corpo = [nova_cabeca] + self.corpo
-    
     #def gameOver(self):

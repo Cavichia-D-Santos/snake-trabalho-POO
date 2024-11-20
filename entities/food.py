d8e87e375d131aae9cc70fc7a7089d5cdc619df0
ManoSoraDev <thiagogamer10000@gmail.com>
2024-11-20 19:23:16 -0300

    Separacao das acoes da comida

--- entities/food.py
+++ entities/food.py
@@ -3,16 +3,14 @@ import random #Biblioteca para gerar um número aleatorio em um intervalo
 
 class Food:
     def __init__(self):
-        self.x = random.randint(0, 31) * 20 #Gera uma posicao x inicial
-        self.y = random.randint(0, 31) * 20 #Gera uma posicao y inicial
         self.cor = (255, 0, 0)
+        self.x = None
+        self.y = None
+        self.food_coord()
 
-    def food_tela(self, screen, consumido):
-        if consumido:
-            self.x = random.randint(0, 31) * 20  # Nova posicao x
-            self.y = random.randint(0, 31) * 20  # Nova posicao y
+    def food_coord(self):
+        self.x = random.randint(0, 31) * 20  # Nova posicao x
+        self.y = random.randint(0, 31) * 20  # Nova posicao y
 
-        py.draw.rect(screen, self.cor, py.Rect(self.x, self.y, 20, 20))
-
-
-    #def consumida(self, snake):
+    def desenhar(self, screen):
+        py.draw.rect(screen, self.cor, (self.x, self.y, 20, 20))
